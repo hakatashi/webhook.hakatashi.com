ceb79bba4ee16da88f658cd3385234e69bc01a0c
Koki Takahashi <hakatasiloving@gmail.com>
2015-03-28 21:55:01 +0900

    Implement everything

---
+++ hooks/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+    res.sendFile('index.html', {
+        root: __dirname + '/..',
+    });
+});
+
+module.exports = router;
