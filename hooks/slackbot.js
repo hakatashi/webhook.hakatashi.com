73fba975f862eefc963587af40817e73aa6ea33e
Koki Takahashi <hakatasiloving@gmail.com>
2017-10-17 23:01:32 +0900

    Implement hooking

--- hooks/slackbot.js
+++ hooks/slackbot.js
@@ -1,4 +1,6 @@
 const createHandler = require('github-webhook-handler');
+const {exec} = require('child_process');
+const {promisify} = require('util');
 const config = require('../config').hooks.slackbot;
 
 const handler = createHandler({
@@ -14,8 +16,16 @@ handler.on('ping', (event) => {
 	console.log(event);
 });
 
-handler.on('push', (event) => {
+handler.on('push', async (event) => {
 	console.log(event);
+	const branch = event.payload.ref.split('/').pop();
+	if (event.payload.repository.name === 'slackbot' && branch === 'master') {
+		console.log('Hooking slackbot...');
+		const {stdout, stderr} = await promisify(exec)('cd /home/hakatashi/slackbot && node deploy.js');
+		console.log('Hooked slackbot.');
+		console.log(`stdout:\n${stdout}\n`);
+		console.log(`stderr:\n${stderr}\n`);
+	}
 });
 
 module.exports = handler;
